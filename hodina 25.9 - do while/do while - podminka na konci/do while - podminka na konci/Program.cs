46e3f10def1195d30eca2ca3c6d41523fba98612
honzik-byte <prusan21@gmail.com>
2024-12-04 16:06:17 +0100

    Add files via upload

---
+++ hodina 25.9 - do while/do while - podminka na konci/do while - podminka na konci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace do_while___podminka_na_konci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string zadecislo = "Zadej číslo";
+            int numberint;
+
+            
+            do
+            {
+               
+            }
+            while (!int.TryParse(Console.ReadLine(), out numberint));
+
+            Console.WriteLine(zadecislo);
+            if (!int.TryParse(Console.ReadLine(), out numberint))
+            {
+                Console.WriteLine("Neplatný vstup, zadejte celá čísla");
+            }
+        }
+    }
+}
---
+++ hodina 25.9 - do while/hodina 4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hodina_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string zadecislo = "Zadej číslo";
+            int numberint;
+            Console.WriteLine(zadecislo);
+            int.Parse("2");
+            while (!int.TryParse(Console.ReadLine(), out numberint)) ;
+            {
+                Console.WriteLine("Neplatný vstup, zadejte celá čísla");
+            }
+        }
+    }
+}
