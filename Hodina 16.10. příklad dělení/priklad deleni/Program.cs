fc88c558c1e485b8ca2c02548084428deb780e13
honzik-byte <prusan21@gmail.com>
2024-12-04 16:07:13 +0100

    Add files via upload

---
+++ Hodina 16.10. příklad dělení/priklad deleni/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace priklad_deleni                                //***Názvy začínají velkým písmenem (proměnné malým)
+                                                        //***Rozdělte program na jednotlivé úlohy 
+                                                        //***Ošetřit vstupy
+{
+    internal class Program
+    {
+        static void Main(string[] args)                 // ***Zobrazovací texty uložte na začátek do const string/int JMeno =" Obsah", konstanty se píšou na začátku s velkými písmeny
+        {
+            //deklarace proměnných
+            string str;
+            int num;
+
+            //získání vstupu od uživatele
+            Console.WriteLine("Zadejte řetězec (string):");
+            str = Console.ReadLine();
+
+            Console.WriteLine("Zadejte velé číslo (int): ");
+            num = int.Parse(Console.ReadLine());
+
+            //výměna hodnot mezi proměnnými
+            string tempStr = str;
+            str = num.ToString();
+            num = int.Parse(tempStr);
+
+            // Zobrazení vyměněných hodnot
+            Console.WriteLine($"\nPo výměně: \nString: {str}\nInt: {num}");     //***Vysvětlete řádek
+
+            // Dělení čísel 
+            Console.Write("\nZadejte další číslo pro dělení: ");
+            int divider = int.Parse(Console.ReadLine());
+
+            // Vypis výsledku s popisem
+            if (divider != 0)
+            {
+                double result = (double)num / divider;
+                Console.WriteLine($"\nVýsledek dělení {num} / {divider} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("\n Ahoj, neumíš matematiku, když chceš dělit nulou ty ňoumo. Doporučuju doučování u Holubové.");
+            }
+    }
+}
+}
+
---
+++ Hodina 9.10 - Start 24/hodina 9.10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hodina_9._10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
