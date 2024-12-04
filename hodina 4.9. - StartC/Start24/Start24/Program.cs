fd2208899ba9709a73c3168122c958c143bb0ebf
honzik-byte <prusan21@gmail.com>
2024-12-04 16:04:53 +0100

    Add files via upload

---
+++ hodina 4.9. - StartC/Start24/Start24/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.WriteLine(1+1);
+Console.WriteLine("1" + "1");
+int jedna;
+jedna = 2;
+Console.Write(jedna + jedna);
+
+string str1 = "Ahoj";
+string str2 = "Darebáku";
+string result = str1 + str2;
+Console.WriteLine(result);
+string jmeno;
+char znak;
+char znak1;
+char result2 = (znak + znak1);
+
+
