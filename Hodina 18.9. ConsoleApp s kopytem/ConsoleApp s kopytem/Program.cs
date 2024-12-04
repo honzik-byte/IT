1180f22e12acf6b0f020c107bc32ac63cc67142b
honzik-byte <prusan21@gmail.com>
2024-12-04 16:05:38 +0100

    Add files via upload

---
+++ Hodina 18.9. ConsoleApp s kopytem/ConsoleApp s kopytem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp_s_kopytem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int index = 1;
+            if (index == 0)
+                Console.WriteLine("Je nula");
+            else
+                Console.WriteLine("Není nula");
+
+        }
+    }
+}
+
+namespace Zkouska
+{
+
+
+
+}
